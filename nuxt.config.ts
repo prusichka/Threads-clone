c2d3769df66ae75061688fdf87c538c1c82ff481
prusichka <denchuk.prusak@gmail.com>
2023-09-03 17:25:07 +0300

    add layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: true,
+  pages: true,
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+  devtools: { enabled: false },
+  modules: [
+    "nuxt-icon",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vite-pwa/nuxt",
+  ],
+});
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <Head>
+    <Title>Threads</Title>
+  </Head>
+</template>
